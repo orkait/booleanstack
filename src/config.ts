a87bf85afe21279d01860be78893647cc60cbcd8
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2025-01-10 09:02:16 +0530

    sync update

--- src/config.ts
+++ src/config.ts
@@ -1,30 +1,32 @@
 export const SITE_FAVICON = "/favicon.png";
-export const SITE_LOGO = "/avatar.png";
-export const SITE_TITLE = "Mia";
-export const SITE_DESCRIPTION =
-  "Mia is designed to be minimalist yet powerful, integrating the capabilities of Astro, MDX, and Rough Notation. Whether you're a developer, writer, or just someone who loves clean and efficient design, this theme is perfect for you.";
+export const SITE_LOGO = "/logo.svg";
+export const SITE_TITLE = "BooleanStack";
+export const SITE_DESCRIPTION = `
+  Mia is designed to be minimalist yet powerful, integrating the capabilities of Astro, MDX, 
+  and Rough Notation. Whether you're a developer, writer, 
+  or just someone who loves clean and efficient design, this theme is perfect for you.`;
 
 export const MENUS = [
-  {
-    title: "Posts",
-    link: "/posts",
-    target: "_self",
-  },
-  {
-    title: "Tags",
-    link: "/tags",
-    target: "_self",
-  },
-  {
-    title: "Archive",
-    link: "/date",
-    target: "_self",
-  },
-  {
-    title: "GitHub",
-    link: "https://github.com/infinity-ooo/astro-theme-mia",
-    target: "_blank",
-  },
+    {
+        title: "Posts",
+        link: "/posts",
+        target: "_self",
+    },
+    {
+        title: "Tags",
+        link: "/tags",
+        target: "_self",
+    },
+    {
+        title: "Archive",
+        link: "/date",
+        target: "_self",
+    },
+    {
+        title: "GitHub",
+        link: "https://github.com/infinity-ooo/astro-theme-mia",
+        target: "_blank",
+    },
 ];
 
 export const FOOTER_CONTENT = "Made by Humans for Superhumans 🚀";
