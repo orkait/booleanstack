fd04f5a190219419fb82e8752345ed8a07093aae
orkait <orkaitsolutions@gmail.com>
2025-01-21 00:26:29 +0530

    sync update

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,15 +3,15 @@ import { getCollection } from "astro:content";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../config";
 
 export async function GET(context: any) {
-  const posts = await getCollection("blog");
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      pubDate: new Date(post.data.date),
-      link: `/blog/${post.slug}/`,
-    })),
-  });
+    const posts = await getCollection("blog");
+    return rss({
+        title: SITE_TITLE,
+        description: SITE_DESCRIPTION,
+        site: context.site,
+        items: posts.map((post) => ({
+            ...post.data,
+            pubDate: new Date(post.data.date),
+            link: `/blog/${post.slug}/`,
+        })),
+    });
 }
